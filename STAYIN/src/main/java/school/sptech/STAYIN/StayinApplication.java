3cf25c89e62dea6ca00c5c4ae6d4363c5df1e589
ViSuliman <vitoria.suliman@sptech.school>
2025-02-25 19:29:47 -0300

    estrutura do projeto em SpringBoot

---
+++ STAYIN/src/main/java/school/sptech/STAYIN/StayinApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.STAYIN;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayinApplication.class, args);
+	}
+
+}
